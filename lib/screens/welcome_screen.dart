a3bc3bfde0852513c280fbe1ae9bbcc1341482eb
pramodksh <pramodkumarsh3@gmail.com>
2022-09-24 16:23:08 +0530

    added animated_text_kit and animated Flash Chat text in welcome_screen.dart

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,21 +1,51 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flash_chat_flutter/main.dart';
 import 'package:flash_chat_flutter/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
 import 'login_screen.dart';
 
 class WelcomeScreen extends StatefulWidget {
-
   static String id = 'welcome_screen';
+
 
   @override
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController? controller;
+  Animation ?animation;
+  int animationDuration = 2;
+
+  // TODO: Customize Animation
+
+  @override
+  void initState() {
+
+    super.initState();
+
+    controller = AnimationController(vsync: this, duration: Duration(seconds: animationDuration));
+    animation = ColorTween(begin: Colors.black26, end: Colors.white).animate(controller!);
+    // Animation animation = CurvedAnimation(parent: controller!, curve:Curves.decelerate);
+
+    controller?.forward();
+    // controller?.reverse(from: 100);
+
+
+    controller?.addListener(() {
+      // print((animation.value * 100).toInt());
+      print(animation?.value);
+      setState((){});
+    });
+
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: animation?.value,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
@@ -28,16 +58,28 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                   tag: 'logo',
                   child: Container(
                     child: Image.asset('images/logo.png'),
-                    height: 60.0,
+                    height: 60,
                   ),
                 ),
-                Text(
-                  'Flash Chat',
+                DefaultTextStyle(
                   style: TextStyle(
-                    fontSize: 45.0,
-                    fontWeight: FontWeight.w900,
+                        fontSize: 45.0,
+                        fontWeight: FontWeight.w900,
+                        color: Colors.black,
+                      ),
+                  child: AnimatedTextKit(
+                    animatedTexts: [
+                      TypewriterAnimatedText('Flash Chat'),
+                    ],
                   ),
                 ),
+                // Text(
+                //   'Flash Chat',
+                //   style: TextStyle(
+                //     fontSize: 45.0,
+                //     fontWeight: FontWeight.w900,
+                //   ),
+                // ),
               ],
             ),
             SizedBox(
